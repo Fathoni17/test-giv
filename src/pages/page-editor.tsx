364be01424959c0b49ef334d44a45795fce99938
Muh. Fathoni N <mfathonin@gmail.com>
2021-07-25 16:18:43 +0700

    Create header

---
+++ src/pages/page-editor.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { HeaderContainer } from '../components'
+
+export const PageEditor = () => {
+    return (
+        <>
+            <HeaderContainer />
+            <div>
+                {/* <MenuBar /> */}
+                <div>
+                    {/* <NavIndicator /> */}
+                    <div className='main-container'>
+                        <div className="main-pane">
+
+                        </div>
+                        <div className='side-pane'></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
